914ae5e3791662c160310779ac9a1758e33141ec
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2025-05-13 21:27:04 +0530

    Setting My Netflix GPT Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      Namaste everyone, let's build netflix-gpt.
+      Hello buddy
+    </div>
+  );
+}
+export default App; 
