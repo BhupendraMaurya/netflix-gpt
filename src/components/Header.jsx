f928277172b524a72152333c37302a2f060f46ad
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2025-05-16 09:20:45 +0530

    Login Form Created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 import netFlixLogo from '../assets/Netflix_Logo_PMS.png';
 const Header = () =>{
     return (
-      <div className="absolute px-8 py-2 bg-gradient-to-b from-black
-">
+      <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
         <img 
         className="w-45"
         src={netFlixLogo}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,13 +2,39 @@ import Header from "./Header";
 import netflixBg from '../assets/Netflix_Background_Image.jpg';
 
 const Login = () => {
-    return <div>
-        <Header/>
-            <div><img
-                src={netflixBg}
-                alt="Netflix background"
-                />
-            </div>
-        </div>;
+  return (
+    <div>
+      <Header />
+
+      {/* Keep your original background image layout */}
+      <div className="absolute">
+        <img
+          src={netflixBg}
+          alt="Netflix background"
+        />
+      </div>
+
+      {/* Centered form without affecting the background */}
+      <form className="w-3/12 absolute p-12 bg-black/80 top-1/4 left-1/2 transform -translate-x-1/2 text-white rounded-md">
+        <h1 className="font-bold text-3xl pb-6">Sign In</h1>
+
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-3 mb-4 w-full text-black bg-gray-700 rounded"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 mb-6 w-full text-black bg-gray-700 rounded"
+        />
+
+        <button className="py-3 bg-red-600 text-white rounded lg w-full hover:bg-red-700 transition">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 };
+
 export default Login;
