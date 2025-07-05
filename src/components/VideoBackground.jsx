0579cc5fa0682d389e396db9b1279912dc78f917
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2025-07-05 13:03:12 +0530

    updated custom hook

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,30 +1,11 @@
-import { useEffect } from 'react'
 import { API_OPTIONS } from '../utils/constants';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTrailerVideo } from "../utils/moviesSlice";
+import {useSelector } from 'react-redux';
+import useMovieTrailer from '../hooks/useMovieTrailer';
 
 const VideoBackground = ({movieId}) => {
   const trailerVideo = useSelector(store => store.movies?.trailerVideo);
-  const dispatch = useDispatch();
 
-  //fetch triler video
-
-  const getMovieVideos = async () =>{
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, 
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-
-    const filterData = json.results.filter((video) => video.type == "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+  useMovieTrailer(movieId);
 
   return (
     <div>
--- src/hooks/useMovieTrailer.jsx
+++ src/hooks/useMovieTrailer.jsx
@@ -4,30 +4,26 @@ import { API_OPTIONS } from '../utils/constants';
 import { addTrailerVideo } from '../utils/moviesSlice';
 
 const useMovieTrailer = (movieId) => {
-  // use for taking the data from store
-    const dispatch = useDispatch();
-    // const [trailerId, setTrailerId] = useState(null); // one of the ways to get the trailerId., but now insead of it. we are using the superpower of redux store.
-    // Fetch trailer video & updating the store with the trailer data.
-  
-    const getMovieVideos = async () =>{
+  const dispatch = useDispatch();
 
-        if (!movieId) return; // ✅ Do not fetch if movieId is invalid
+  //fetching trailer video and updating the store with trailer video data
 
-      const data = await fetch(
-        `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, 
-        API_OPTIONS
-      );
-      const json = await data.json();
-  
-      const filterData = json.results.filter(video => video.type ==="Trailer");
-      const trailer = filterData.length ? filterData[0] : json.results[0];
 
-      dispatch(addTrailerVideo(trailer));
-    };
-  
-    useEffect(() =>{
-      getMovieVideos();
-    },[movieId]);
+  const getMovieVideos = async () =>{
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, 
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    const filterData = json.results.filter((video) => video.type == "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+
+    dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
 }
 
 export default useMovieTrailer;
